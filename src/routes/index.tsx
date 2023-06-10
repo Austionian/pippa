d6624e99e1057c53a41a37c87fc7eb2266731968
Austin Rooks <Austionian@users.noreply.github.com>
2023-06-09 20:39:52 -0500

    motion one

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,47 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { animate } from "motion";
 
 export default component$(() => {
+  useVisibleTask$(() => {
+    const img = document.querySelector("#pippa");
+    if (img) {
+      animate(
+        img,
+        {
+          transform: [
+            "rotate(0deg)",
+            "rotate(-45deg)",
+            "rotate(-90deg)",
+            "rotate(-135deg)",
+            "rotate(-180deg)",
+            "rotate(-225deg)",
+            "rotate(-270deg)",
+            "rotate(-315deg)",
+            "rotate(-360deg)",
+          ],
+        },
+        {
+          duration: 7,
+          easing: "ease-in-out",
+          repeat: Infinity,
+          delay: 0.3,
+        }
+      );
+    }
+  });
+
   return (
     <div class="flex items-center justify-center h-screen w-screen flex-col">
       <h1 class="p-10 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         DUMB BITCH NAMED PIPPA .com
       </h1>
-      <img src={`/images/pippa.png`} alt="pippa" class="h-[75%] w-[75%]" />
+      <img
+        src={`/images/pippa.png`}
+        alt="dumb bitch"
+        class="h-[75%] w-[75%]"
+        id="pippa"
+      />
     </div>
   );
 });
